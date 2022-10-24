cc853198506f2b2b6d5c1eece102a06f67f29e69
Doomitrii <podorvanov-dmitrii@yandex.ru>
2022-10-24 20:53:21 +0500

    Box

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+return (
+    <div className='header'>
+        <div className="container">
+           <div className="header_inner">
+               <div className="header__logo">
+                   <img src='./img/1149099dbf954efd9cbca16f6218c135.jpg' alt='logo'/>
+               </div>
+               <div className="nav">
+                   <a className="nav__link" href='#'>
+                       spooky
+                   </a>
+                   <a className="nav__link" href='#'>
+                       scary
+                   </a>
+                   <a className="nav__link" href='#'>
+                       skeletons
+                   </a>
+                   <a className="nav__link" href='#'>
+                       Send shivers
+                   </a>
+                   <a className="nav__link" href='#'>
+                       down your spine
+                   </a>
+
+               </div>
+           </div>
+        </div>
+    </div>
+);
+}
+export default App;
